695547e00a5bea9eb2bf08f8ef1584d8b3d34ddf
marga227 <aurazambrano22@gmail.com>
2021-02-04 22:50:40 +0000

    Fixing string error

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -19,13 +19,13 @@ if (n > 0)
 {
 	printf("%i is positive\n", n);
 }
-else if (n == 0)
-{
-	printf("%i is zero\n", n);
-}
-else (n < 0)
+else if (n < 0)
 {
 	printf("%i is negative\n", n);
+}
+else
+{
+	printf("%i is zero\n", n);
 }
 	return (0);
 }
