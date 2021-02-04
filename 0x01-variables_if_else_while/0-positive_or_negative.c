4dc1ef1dcb4bea8ba4a9a5764a162dfc9e41b4cb
marga227 <aurazambrano22@gmail.com>
2021-02-04 22:01:48 +0000

    Add task positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,23 +3,28 @@
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
+/**
+ *main -Entry point
+ *
+ *Return: Alwais 0 (Sucess)
+ */
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if(n>0)
-        {
-        printf("is positive\n");
-	}
-        else if(n==0)
-	{
+if (n > 0)
+{
+	printf("is positive\n");
+}
+else if (n == 0)
+{
 	printf("is zero\n");
-	}
-	else if(n<0)
-	{
-	printf("is negative\n");
-	}
+}
+else if (n < 0)
+{
+printf("is negative\n");
+}
 	return (0);
 }
