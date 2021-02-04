b26b78236106daddb64f605c87d968ac2728ebac
marga227 <aurazambrano22@gmail.com>
2021-02-04 16:05:53 +0000

    Modified task about puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+
 /**
  * main - Entry point
  *
@@ -7,10 +8,8 @@
  */
 int main(void)
 {
- 
- 
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
 
 
