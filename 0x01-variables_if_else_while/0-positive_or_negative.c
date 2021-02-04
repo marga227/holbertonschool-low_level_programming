e59fa8cbb7112b8b0f4cca0c4537e6d70c249fc2
marga227 <aurazambrano22@gmail.com>
2021-02-04 22:38:11 +0000

    Modified data type

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -16,15 +16,15 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 if (n > 0)
 {
-	printf("is positive\n");
+	printf("%i is positive\n", n);
 }
 else if (n == 0)
 {
-	printf("is zero\n");
+	printf("%i is zero\n", n);
 }
 else (n < 0)
 {
-printf("is negative\n");
+	printf("%i is negative\n", n);
 }
 	return (0);
 }
