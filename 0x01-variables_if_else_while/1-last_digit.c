449eca7075d2b31bf15420a8b3aa5ed6ade02e90
marga227 <aurazambrano22@gmail.com>
2021-02-04 21:37:36 +0000

    Add task positive or negative

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if(n>5)
+	}
+	printf("Last digit of $n is and is greater than  5\n")
+	}
+	else if(
+	return (0);
+}
