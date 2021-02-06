f3a7cc6928af694641a8b099120dcd30162139b3
marga227 <aurazambrano22@gmail.com>
2021-02-06 03:55:33 +0000

    Fixed bugs betty task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,17 +7,17 @@
  */
 int main(void)
 {
-char char_type
-int int_type;
-long int long_int_type;
-long long type long2_type;
-float float_type;
+char a;
+int b;
+long int c;
+long long int d;
+float e;
 
     /*sizeof evaluates the size of a variable*/
-printf("Size of a char: %zu byte(s)\n", sizeof(char_type));
-printf("Size of a int: %zu byte(s)\n", sizeof(int_type));
-printf("Size of a long int: %zu byte(s)\n", sizeof(long_int_type));
-printf("Size of a long long int: %zu byte(s)\n", sizeof(long2_type));
-printf("Size of a float: %zu byte(s)\n", sizeof(float_type));
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
+printf("Size of a int: %lu byte(s)\n", (unsigned long)sizeof(b));
+printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
+printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(e));
 return (0);
 }
