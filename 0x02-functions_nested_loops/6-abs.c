fa4bb557a0bb7a591edbe41e98786106570bd2cf
marga227 <aurazambrano22@gmail.com>
2021-02-09 23:58:45 +0000

    Add exercise 6 absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+  * _abs - Function that computes the absolutes value of an integer.
+  * Return: Always 0.
+  * @i: Number to check.
+  */
+
+int _abs(int i)
+{
+	if (i < 0)
+	i = i * -1;
+
+	else
+	i = i;
+
+
+return (i);
+
+}
