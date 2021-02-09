9f169007363f61f1dc8ec50edad19482884c5177
marga227 <aurazambrano22@gmail.com>
2021-02-09 16:31:52 +0000

    Add task 0 holberton

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * main - Program to print string "Holberton".
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+
+char name[] = "Holberton";
+int a;
+
+for (a = 0 ; a <= 9 ; a++)
+
+{
+	_putchar(name[a]);
+}
+
+_putchar('\n');
+return (0);
+}
