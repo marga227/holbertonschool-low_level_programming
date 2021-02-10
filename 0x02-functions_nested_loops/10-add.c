fda1c3fc1e8109082144e4c9e1361310c0cab332
marga227 <aurazambrano22@gmail.com>
2021-02-10 04:43:40 +0000

    Add exercise number ten

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+  * add - Program that adds two integers and returns the result.
+  * Return: Add two integers.
+  * @num1: Number to add.
+  * @num2: Number to add.
+  */
+
+int add(int num1,  int num2)
+{
+
+int a;
+a = num1 + num2;
+
+return (a);
+
+}
