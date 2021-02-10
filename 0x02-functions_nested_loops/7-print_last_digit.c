702bba466b1a07645288fbf342dd10601eac87a3
marga227 <aurazambrano22@gmail.com>
2021-02-10 04:09:00 +0000

    Add exercise last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+  * print_last_digit - Function that prints the last digit of a number.
+  * Return: last.
+  * @n: Number to check.
+  */
+
+int print_last_digit(int n)
+
+{
+
+	if (n < 0)
+	{
+		n =  (n % 10) * -1;
+	}
+
+	_putchar((n % 10) + '0');
+
+
+
+return (n);
+}
