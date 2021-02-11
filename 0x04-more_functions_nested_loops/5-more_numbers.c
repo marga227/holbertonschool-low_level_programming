579ece87c4476c7c8822ec28af8c1e132579aa51
marga227 <aurazambrano22@gmail.com>
2021-02-11 19:30:30 +0000

    Add exercise more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+/* Produced by Aura Margarita Zambrano */
+/**
+ *more_numbers -Program that prints numbers 0-9.
+ *
+ *Return: Nothing.
+ */
+void more_numbers(void)
+{
+	int a;
+	int b;
+
+	for (a = 0 ; a <= 10; a++)
+	{
+		for (b = 1 ; b <= 14 ; b++)
+		{
+		_putchar (b % 10 + '0');
+
+			if (b > 9 && b < 14)
+			{
+			_putchar (b / 10 + '0');
+			}
+		}
+
+	_putchar ('\n');
+	}
+
+}
+
