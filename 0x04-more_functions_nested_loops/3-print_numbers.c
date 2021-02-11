3f04372dc96377b9f2a21c2d4a3274fcad4157d6
marga227 <aurazambrano22@gmail.com>
2021-02-11 19:26:20 +0000

    Add exercise print numbers to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/* Produced by Aura Margarita Zambrano */
+/**
+ *print_numbers -Program that prints numbers 0-9.
+ *
+ *Return: Nothing.
+ */
+void print_numbers(void)
+{
+
+	int a;
+for (a = 48 ; a <= 57 ; a++)
+	_putchar (a);
+
+	_putchar ('\n');
+
+	return;
+}
+
