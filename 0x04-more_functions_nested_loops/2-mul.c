e5397255737dac92639b24103e5ba80349737bfe
marga227 <aurazambrano22@gmail.com>
2021-02-11 19:23:59 +0000

    Add exercise multiplies

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+  * mul - Program that adds two integers and returns the result.
+  * Return: Add two integers.
+  * @a: Number to mul.
+  * @b: Number to mul.
+  */
+
+int mul(int a,  int b)
+{
+
+int z;
+z = a * b;
+
+return (z);
+
+}
