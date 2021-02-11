a9f9eba377850333a7901825480b50341c134143
marga227 <aurazambrano22@gmail.com>
2021-02-11 19:28:35 +0000

    Add exercise print most numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/* Produced by Aura Margarita Zambrano */
+/**
+ *print_most_numbers -Program that prints numbers 0-9.
+ *
+ *Return: Nothing.
+ */
+void print_most_numbers(void)
+{
+	int a;
+for (a = 48 ; a <= 57 ; a++)
+{
+if (a != 50 && a != 52)
+	_putchar (a);
+}
+	_putchar ('\n');
+
+	return;
+}
+
