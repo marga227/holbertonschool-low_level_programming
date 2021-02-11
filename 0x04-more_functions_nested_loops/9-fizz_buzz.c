b5683cdfb3d0d983a3a7ef65aa7cdffa84ad2873
marga227 <aurazambrano22@gmail.com>
2021-02-11 23:33:56 +0000

    Add exercise fizz buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "holberton.h"
+/* Produced by Aura Margarita Zambrano */
+/**
+ *main -Program that prints numbers 0-100.
+ *
+ *Return: Always 0.
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		printf("FizzBuzz ");
+
+		else if (i % 3 == 0)
+		printf("Fizz ");
+
+		else if (i % 5 == 0)
+		printf("Buzz ");
+
+		else
+		printf("%d ", i);
+	}
+
+
+printf("\n");
+return (0);
+}
