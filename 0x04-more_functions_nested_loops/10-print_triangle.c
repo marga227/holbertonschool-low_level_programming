9b2cecd8f955ce29bc374f9d90cadd6d99a6b0e5
marga227 <aurazambrano22@gmail.com>
2021-02-12 04:02:04 +0000

    Modified betty style of print triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -5,27 +5,37 @@
   * @size: Number to check.
   */
 void print_triangle(int size)
-
 {
-	int a;
-	int b;
-	int c;
+  int a;
+  int b;
+  int c;
 
-	for (a = 0; a <= size - 1; a++) /**cant de espa**/
+
+	if (size <= 0)
 	{
-
-		for (c = size ; c > a; c--) /**cant de espa**/
-		{
-		_putchar(' ');
-		}
-
-		for (b = 0; b <= a; b++) /**cant de espa**/
-		{
-		_putchar('#');
-		}
-
-		_putchar('\n');
-
+	_putchar('\n');
 	}
 
+
+
+		for (a = 0; a <= size - 1; a++) /**cant de espa**/
+		{
+
+			for (c = size - 1 ; c > a; c--) /**cant de espa**/
+			{
+			_putchar(' ');
+			}
+
+			for (b = 0; b <= a; b++) /**cant de espa**/
+			{
+			_putchar('#');
+			}
+
+			_putchar('\n');
+
+
+		}
+
+
 }
+
