b9c7f5904b039dc6a802a9afc4d7cadd21577e66
marga227 <aurazambrano22@gmail.com>
2021-02-13 20:37:53 +0000

    Add exercise 10 times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,39 @@
+#include "holberton.h"
+/**
+  * times_table - function that prints the 9 times table.
+  * Return: Nothing.
+  */
+
+void times_table(void)
+{
+	int fact1;
+	int fact2;
+
+	for (fact1 = 0; fact1 < 10; fact1++)
+	{
+	_putchar('0');
+
+		for (fact2 = 0; fact2 < 10; fact2++)
+		{
+			if ((fact1 * fact2) > 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((fact1 * fact2) / 10 + '0');
+				_putchar((fact1 * fact2) % 10 + '0');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar((fact1 * fact2) + '0');
+			}
+		}
+	_putchar('\n');
+	}
+return;
+}
+
+
+
