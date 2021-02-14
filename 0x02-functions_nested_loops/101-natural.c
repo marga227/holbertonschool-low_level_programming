8eb9177e9da6128c7d0d51c91a090b25982c1d0d
marga227 <aurazambrano22@gmail.com>
2021-02-14 01:38:10 +0000

    Modified exercise 101 natural

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-/*
- * main - Program that computes and prints the sum of all the multiples
- * of al the miltiples of 3 or 5 below 1024.
- *
- * Return - Always 0.
- */
 
+/**
+ * main - Prints all the natural numbers below 1024
+ *        that are multiples of 3 or 5.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
 	int i;
@@ -13,14 +13,13 @@ int main(void)
 
 	sum = 0;
 
-	for (i = 0; i < 1024; i++) /*números a chequear*/
+	for (i = 0; i < 1024; i++)
 	{
-		if (i % 3 == 0 || i % 5 == 0)
-		{
-		sum += i;
-		}
-
+		if ((i % 3) == 0 || (i % 5) == 0)
+			sum += i;
 	}
+
 	printf("%i\n", sum);
+
 	return (0);
 }
