3890d9d515c92d3af5db3779bbb156ad4e9a6d5c
marga227 <aurazambrano22@gmail.com>
2021-02-17 03:14:04 +0000

    Add exercise 1 swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+  * swap_int - Swaps the values of two integers.
+  * Return: Nothing.
+  * @a: change to b.
+  * @b: change to a.
+  */
+
+void swap_int(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+
+
+
+}
